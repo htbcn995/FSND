bdba2b7a5649a6060b958443020a5a627d3741e6
kenji fukushima <fukushimakenjii@gmail.com>
2020-12-26 15:42:49 +0900

    db connect set up

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -22,7 +22,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
-
+db.init_app(app)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#

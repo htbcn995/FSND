f7a2229bb870d84e47b8dc05ad9cca233e70784d
kenji fukushima <fukushimakenjii@gmail.com>
2021-03-05 07:02:14 +0900

    fix models.py

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -56,8 +56,8 @@ class Artist(db.Model):
 class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=True)
     start_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
